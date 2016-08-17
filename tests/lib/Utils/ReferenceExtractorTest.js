9ac528a4d4a08100d78068ea6072ecdd17205182
Roberto <iseme.beto@gmail.com>
2016-08-17 16:35:40 -0300

    make ReferenceExtractor recursive

--- tests/lib/Utils/ReferenceExtractorTest.js
+++ tests/lib/Utils/ReferenceExtractorTest.js
@@ -20,16 +20,39 @@ describe('ReferenceExtractor', () => {
             }, ReferenceExtractor.getReferences(fields));
         })
 
-        it('should filter out non-reference fields', () => {
+        it('should filter out non-reference fields and handle embedded_list nesting', () => {
             const fields = [
                 { type() { return 'reference' }, name() { return 'foo' } },
                 { type() { return 'reference_many' }, name() { return 'bar' } },
                 { type() { return 'referenced_list' }, name() { return 'baz' } },
                 { type() { return 'string' }, name() { return 'boo' } }
             ];
+
             assert.deepEqual({
                 foo: fields[0],
                 bar: fields[1]
+            }, ReferenceExtractor.getReferences(fields));
+        })
+
+        it('should handle embedded_list reference nesting', () => {
+            const _joca_foo = {type() { return 'reference'}, name() { return 'joca_foo'}};
+
+            const fields = [
+                { type() { return 'reference' }, name() { return 'foo' } },
+                { type() { return 'reference_many' }, name() { return 'bar' } },
+                { type() { return 'referenced_list' }, name() { return 'baz' } },
+                { type() { return 'embedded_list'}, name() { return 'joca'}, targetFields(){
+                    return [
+                        _joca_foo,
+                        {type() { return 'string'}, name() { return 'joca_boo'}}
+                    ]
+                }}
+            ];
+
+            assert.deepEqual({
+                foo: fields[0],
+                bar: fields[1],
+                joca_foo: _joca_foo
             }, ReferenceExtractor.getReferences(fields));
         })
     })
